004d54903bc648e56b325bc1ee936017222cd7fb
paulrsapio-maker <paulrsapio@gmail.com>
2025-08-13 17:28:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Hello world project</title>
+    <title>Payment Plan Calculator</title>
   </head>
-
   <body>
     <div id="root"></div>
-
-    <script type="module" src="/client/App.tsx"></script>
+    <script type="module" src="/client/main.tsx"></script>
   </body>
 </html>
